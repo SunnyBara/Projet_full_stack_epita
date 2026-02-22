f3ef7b20535891c5c4805dbc9c0f7b683e67a8c7
Romain Vidal <romain1.vidal@epita.fr>
2026-02-22 22:16:43 +0100

    Add default project + DockerFiles

---
+++ backend/src/main/java/fr/epita/backend/controller/TestController.java
@@ -0,0 +1,12 @@
+package fr.epita.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String HelloWorld() {
+        return "Hello World!";
+    }
+}
---
+++ frontend/src/app/app.ts
@@ -0,0 +1,21 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+
+export class App {
+  protected readonly title = signal('frontend');
+
+  protected readonly text = signal("")
+
+  async onButtonClick() {
+    const result = await fetch("http://localhost:8080")
+    console.log(result.url)
+    this.text.set(await result.text())
+  }
+}
